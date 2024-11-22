8a7adad4861c02115cae702c65ee932843a75a84
andrea1932734 <andreacarovelli@gmail.com>
2024-11-22 11:21:35 +0000

    15.2

--- routing-test-app/src/app/app-routing.module.ts
+++ routing-test-app/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { FruitsComponent } from './fruits/fruits.component';
 import { AnimalComponent } from './animal/animal.component';
 import { GenericComponent } from './generic/generic.component';
+import { FooComponent } from './foo/foo.component';
 
 
 const Routes: Routes = [
   { path: 'animals', component: AnimalComponent},
   {path: '', redirectTo: '/animals', pathMatch: 'full'},
   { path: 'generic/:id', component: GenericComponent },
-  { path: 'fruits', component: FruitsComponent}
+  { path: 'fruits', component: FruitsComponent},
+  { path: 'foo', component: FooComponent},
 ];
 
 @NgModule({
